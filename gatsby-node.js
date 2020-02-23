81e18f1a3dc58cc3920f8419540abe4239a3708a
DeadIntegral <jegok@naver.com>
2020-02-23 22:42:32 +0900

    Revert "Docs weekly"

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,31 @@
 const path = require(`path`)
-const _ = require("lodash")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const tagTemplate = path.resolve(`./src/templates/tags.js`)
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { fields: [frontmatter___date], order: DESC }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              tags
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
             }
           }
         }
       }
-      tagsGroup: allMarkdownRemark(limit: 1000) {
-        group(field: frontmatter___tags) {
-          fieldValue
-        }
-      }
-    }
-  `)
+    `
+  )
 
   if (result.errors) {
     throw result.errors
@@ -50,17 +45,6 @@ exports.createPages = async ({ graphql, actions }) => {
         slug: post.node.fields.slug,
         previous,
         next,
-      },
-    })
-  })
-
-  const tags = result.data.tagsGroup.group
-  tags.forEach(tag => {
-    createPage({
-      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
       },
     })
   })
